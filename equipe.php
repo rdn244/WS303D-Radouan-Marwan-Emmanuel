a19028509ab51e63c880708728396e113523dc5f
rdn244 <73.beepers.daim@icloud.com>
2025-10-22 09:32:46 +0200

    Add files via upload

---
+++ equipe.php
@@ -0,0 +1,85 @@
+<?php
+include("header.php");
+?>
+<link rel="stylesheet" href="css/coord.css">
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+<div class="block_principale block">
+    <h1>Nos coordonnées</h1>
+
+    <div id="fiche_coord">
+        <!-- ============================== -->
+        <!-- 👤 Fiche 1 : Marwan -->
+        <!-- ============================== -->
+        <div id="marwan">
+            <div class="pp_nom">
+                <div class="avatar_marwan"></div>
+                <div class="nom_fiche">
+                    <h4>Marwan</h4>
+                    <h4>Bouchebbat</h4> <!-- Nom de famille laissé vide -->
+                </div>
+            </div>
+
+            <div class="coord_perso">
+                <a href="tel:">
+                    <img class="icone_coord" src="img/icones/telephone.png" alt="image téléphone">
+                  07 67 11 34 14
+                </a>
+                <a href="mailto:">
+                    <img class="icone_coord" src="img/icones/email.png" alt="logo enveloppe email">
+                    marwan.bouchebbat@etudiant.univ-reims.fr
+                </a>
+            </div>
+        </div>
+
+        <!-- ============================== -->
+        <!-- 👤 Fiche 2 : Radouan -->
+        <!-- ============================== -->
+        <div id="radouan">
+            <div class="pp_nom">
+                <div class="avatar_radouan"></div>
+                <div class="nom_fiche">
+                    <h4>Radouan</h4>
+                    <h4>Ouriri</h4> <!-- Nom de famille laissé vide -->
+                </div>
+            </div>
+
+            <div class="coord_perso">
+                <a href="tel:">
+                    <img class="icone_coord" src="img/icones/telephone.png" alt="image téléphone">
+                    07 67 37 92 56
+                </a>
+                <a href="mailto:">
+                    <img class="icone_coord" src="img/icones/email.png" alt="logo enveloppe email">
+                    radouan.ouriri@etudiant.univ-reims.fr
+                </a>
+            </div>
+        </div>
+
+        <!-- ============================== -->
+        <!-- 👤 Fiche 3 : Emmanuel -->
+        <!-- ============================== -->
+        <div id="emmanuel">
+            <div class="pp_nom">
+                <div class="avatar_emmanuel"></div>
+                <div class="nom_fiche">
+                    <h4>Emmanuel</h4>
+                    <h4>Etsion</h4> <!-- Nom de famille laissé vide -->
+                </div>
+            </div>
+
+            <div class="coord_perso">
+                <a href="tel:">
+                    <img class="icone_coord" src="img/icones/telephone.png" alt="image téléphone">
+                    07 ...
+                </a>
+                <a href="mailto:">
+                    <img class="icone_coord" src="img/icones/email.png" alt="logo enveloppe email">
+                    emmanuel.etsion@etudiant.univ-reims.fr
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
